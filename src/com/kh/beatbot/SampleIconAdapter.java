ed4d8c421fe36d73de5ca85b68cb3e74f7d2f92a
Karl Hiner <karl.hiner@gmail.com>
2012-04-29 17:04:00 -0700

    moving stuff around

--- src/com/kh/beatbot/SampleIconAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.kh.beatbot;
-
-import android.R;
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
