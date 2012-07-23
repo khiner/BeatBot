299b10c8c771ef77feaa88d1c68ee43be40588e3
Karl Hiner <karl.hiner@gmail.com>
2012-07-22 21:39:29 -0700

    changing the min length of chorus, and reducing the max delay length to save space

--- jni/effects.c
+++ jni/effects.c
@@ -219,7 +219,7 @@ void filterconfig_destroy(void *p) {
 ChorusConfig *chorusconfig_create(float modFreq, float modAmt) {
 	ChorusConfig *config = (ChorusConfig *)malloc(sizeof(ChorusConfig));
 	chorusconfig_setBaseTime(config, MIN_CHORUS_DELAY + (MAX_CHORUS_DELAY - MIN_CHORUS_DELAY) / 2);
-	config->delayConfig = delayconfigi_create((config->baseTime)*INV_SAMPLE_RATE, .5f, MAX_CHORUS_DELAY + 2048);
+	config->delayConfig = delayconfigi_create((config->baseTime)*INV_SAMPLE_RATE, .5f, MAX_CHORUS_DELAY + 512);
 	config->mod[0] = sinewave_create();
 	config->mod[1] = sinewave_create();
 	chorusconfig_set(config, modFreq, modAmt);
