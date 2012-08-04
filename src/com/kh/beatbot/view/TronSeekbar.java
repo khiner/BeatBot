0a3d8742aad08356b789413f9b6ba1de1f86055f
Karl Hiner <karl.hiner@gmail.com>
2012-08-04 13:52:19 -0700

    switched TronSeekbar to use triangle strips instead of just thick lines - a few more lines of code, but now we have much more control over thickness to allow for selection 'glow', and we just use one FloatBuffer for all level bars, used for both background, level and circles

--- src/com/kh/beatbot/view/TronSeekbar.java
+++ src/com/kh/beatbot/view/TronSeekbar.java
@@ -12,8 +12,8 @@ import com.kh.beatbot.listenable.LevelListenable;
 import com.kh.beatbot.listener.LevelListener;
 
 public class TronSeekbar extends LevelListenable {
-	private FloatBuffer bgBarVb = null;
-	private FloatBuffer levelBarVb = null;
+	private static FloatBuffer levelBarVb = null;
+	private int numLevelVertices = 0;
 
 	public TronSeekbar(Context c, AttributeSet as) {
 		super(c, as);
@@ -23,32 +23,49 @@ public class TronSeekbar extends LevelListenable {
 		levelListeners.add(levelListener);
 	}
 	
-	private void initBgBar() {
-		float[] vertices = new float[] { height/2, height/2, width - height/2, height/2 };
-		bgBarVb = makeFloatBuffer(vertices);		
-	}
-	
 	private void initLevelBarVB() {
-		float[] vertices = new float[] { height/2, height/2, levelToX(level), height/2 };
+		if (levelBarVb != null)
+			return; // only need one VB for all level bars to share
+		float[] vertices = new float[800];
+		for (int i = 0; i < vertices.length / 4; i++) {
+			vertices[i * 4] = ((float) i / (vertices.length / 4)) * (width - 10) + 5;
+			vertices[i * 4 + 1] = -4;
+			vertices[i * 4 + 2] = vertices[i * 4];
+			vertices[i * 4 + 3] = 4;
+		}
 		levelBarVb = makeFloatBuffer(vertices);
 	}
 	
 	@Override
 	protected void init() {
 		super.init();
-		initBgBar();
 		initLevelBarVB();
 	}
 		
-	private void drawBar(FloatBuffer vb, float[] color) {
-		drawLines(vb, color, height/2, GL10.GL_LINES);
-		gl.glPointSize(height/4.5f);
-		gl.glDrawArrays(GL10.GL_POINTS, 0, 2);
-		if (vb.equals(levelBarVb)) {
-			gl.glPointSize(height/2);
-			gl.glColor4f(color[0], color[1], color[2], .7f);
-			gl.glDrawArrays(GL10.GL_POINTS, 1, 1);
-		}
+	private void drawBar() {
+		gl.glPushMatrix();
+		// draw background rect
+		gl.glTranslatef(0,  height/2, 0);
+		drawTriangleStrip(levelBarVb, bgColor);
+		gl.glPointSize(8);
+		
+		// draw circle at the end of background rect
+		gl.glTranslatef(0, 4, 0);
+		gl.glDrawArrays(GL10.GL_POINTS, levelBarVb.capacity() / 2 - 2, 1);
+		gl.glTranslatef(0, -4, 0);
+		
+		// draw level
+		drawTriangleStrip(levelBarVb, levelColor, numLevelVertices);
+		
+		// draw level-colored circle at beginning and end of level
+		gl.glTranslatef(0, 4, 0);
+		gl.glDrawArrays(GL10.GL_POINTS, 0, 1);
+		gl.glDrawArrays(GL10.GL_POINTS, numLevelVertices - 2, 1);
+		// draw bigger, translucent 'selection' circle at end
+		gl.glColor4f(levelColor[0], levelColor[1], levelColor[2], .6f);
+		gl.glPointSize(20);
+		gl.glDrawArrays(GL10.GL_POINTS, numLevelVertices - 2, 1);
+		gl.glPopMatrix();
 	}
 
 	public void setViewLevel(float x) {
@@ -59,10 +76,22 @@ public class TronSeekbar extends LevelListenable {
 	@Override
 	protected void drawFrame() {
 		gl.glClearColor(0, 0, 0, 1);
-		drawBar(bgBarVb, bgColor);
-		drawBar(levelBarVb, levelColor);
+		drawBar();
 	}
 
+	public void setLevel(float level) {
+		super.setLevel(level);
+		updateNumLevelVertices();
+	}
+	
+	private void updateNumLevelVertices() {
+		numLevelVertices = (int)(level*(levelBarVb.capacity() / 2));
+		// want even number of vertices to avoid jagged ending
+		numLevelVertices += numLevelVertices % 2;
+		// make sure we don't go have an out of bounds index
+		numLevelVertices = Math.min(numLevelVertices, levelBarVb.capacity() / 2);
+	}
+	
 	private float xToLevel(float x) {
 		float level = (x - height/2)/(width - height);
 		level = level < 0 ? 0 : (level > 1 ? 1 : level);
@@ -83,5 +112,4 @@ public class TronSeekbar extends LevelListenable {
 	protected void handleActionMove(MotionEvent e) {
 		setLevel(xToLevel(e.getX(0)));
 	}
-
 }
