1ae5d1a788d302a028364c38bac6948671342534
Karl Hiner <karl.hiner@gmail.com>
2012-09-22 17:48:20 -0700

    adapted Wav parsing to allow for mono samples

--- jni/generators/wavfile.c
+++ jni/generators/wavfile.c
@@ -1,30 +1,71 @@
 #include "wavfile.h"
 #include "../track.h"
 
-static inline short charsToShort(unsigned char first, unsigned char second) {
-	return (first << 8) | second;
+static inline float bytesToFloat(unsigned char firstByte,
+		unsigned char secondByte) {
+	// convert two bytes to one short (little endian)
+	short s = (secondByte << 8) | firstByte;
+	// convert to range from -1 to (just below) 1
+	return s / 32768.0;
 }
 
-void wavfile_setBytes(WavFile *wavFile, char *bytes, int length) {
-	// each sample has one short for each channel (2 channels), and 2 bytes per short
-	wavFile->totalSamples = (length - 44) / 4;
+/**
+ * Populate the wavFile float buffers with raw PCM Wav bytes.
+ * Works with mono/stereo (for mono, duplicate each sample to the left/right channels)
+ * Method for detecting/adapting to stereo/mono adapted from
+ * http://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+ */
+void wavfile_setBytes(WavFile *wavFile, char *wav, int length) {
+	// Determine if mono or stereo
+	int channels = wav[22]; // Forget byte 23 as 99.999% of WAVs are 1 or 2 channels
+
+	// Get past all the other sub chunks to get to the data subchunk:
+	int pos = 12; // First Subchunk ID from 12 to 16
+
+	// Keep iterating until we find the data chunk (i.e. 64 61 74 61 ...... (i.e. 100 97 116 97 in decimal))
+	while (wav[pos] != 100 || wav[pos + 1] != 97 || wav[pos + 2] != 116
+			|| wav[pos + 3] != 97) {
+		pos += 4;
+		int chunkSize = wav[pos] + wav[pos + 1] * 256 + wav[pos + 2] * 65536
+				+ wav[pos + 3] * 16777216;
+		pos += 4 + chunkSize;
+	}
+	pos += 8;
+
+	if (channels == 1) {
+		length -= 32; // don't know why the end of mono sample is garbage
+	}
+
+	// pos is now positioned to start of actual sound data.
+	wavFile->totalSamples = (length - pos) / 2; // 2 bytes per sample (16 bit sound mono)
+	if (channels == 2)
+		wavFile->totalSamples /= 2; // 4 bytes per sample (16 bit stereo)
+
 	wavFile->buffers = (float **) malloc(2 * sizeof(float *));
+	// Allocate memory (right will be null if only mono sound)
 	wavFile->buffers[0] = (float *) calloc(wavFile->totalSamples,
 			sizeof(float));
 	wavFile->buffers[1] = (float *) calloc(wavFile->totalSamples,
 			sizeof(float));
+
+	// write to wavFile float buffers
+	int i = 0;
+	while (pos < length) {
+		wavFile->buffers[0][i] = bytesToFloat(wav[pos], wav[pos + 1]);
+		pos += 2;
+		if (channels == 2) {
+			wavFile->buffers[1][i] = bytesToFloat(wav[pos], wav[pos + 1]);
+			pos += 2;
+		} else {
+			wavFile->buffers[1][i] = wavFile->buffers[0][i];
+		}
+		i++;
+	}
+	// init loop / currSample position data
 	wavFile->loopBegin = 0;
-	wavFile->loopEnd = wavFile->totalSamples;
+	wavFile->loopEnd = wavFile->totalSamples - 2;
 	wavFile->currSample = 0;
 
-	int i;
-	for (i = 0; i < wavFile->totalSamples; i++) {
-		// first 44 bytes of a wav file are header
-		wavFile->buffers[0][i] = charsToShort(bytes[i * 4 + 1 + 44],
-				bytes[i * 4 + 44]) * CONVMYFLT;
-		wavFile->buffers[1][i] = charsToShort(bytes[i * 4 + 3 + 44],
-				bytes[i * 4 + 2 + 44]) * CONVMYFLT;
-	}
 	//free(bytes); //taken care of by Release JNI method?
 }
 
