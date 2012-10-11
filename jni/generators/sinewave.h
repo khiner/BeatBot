18941183ee538b54ceb1f35a70ec8b84ffdc4c4d
Karl Hiner <karl.hiner@gmail.com>
2012-10-10 19:43:39 -0700

    removed one comparison and one assignment from sinewave_tick, and converted a floorf to a cast to unsigned int

--- jni/generators/sinewave.h
+++ jni/generators/sinewave.h
@@ -21,19 +21,16 @@ void sinewave_addTimeInPhase(SineWave *config, float phase);
 void sinewave_addPhaseOffset(SineWave *config, float phaseOffset);
 
 static inline float sinewave_tick(SineWave *config) {
-	while (config->time < 0)
-		config->time += TABLE_SIZE;
 	while (config->time >= TABLE_SIZE)
 		config->time -= TABLE_SIZE;
 
-	config->iIndex = floorf(config->time);
+	config->iIndex = (unsigned int)config->time;
 	config->alpha = config->time - config->iIndex;
-	float tmp = config->table[config->iIndex];
-	tmp += (config->alpha * (config->table[config->iIndex + 1] - tmp));
 
 	config->time += config->rate;
 
-	return tmp;
+	return (config->table[config->iIndex] + config->alpha*config->table[config->iIndex + 1]) /
+			(1 + config->alpha);
 }
 
 #endif // SINEWAVE_H
