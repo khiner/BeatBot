572ec002b041d7c47511a3090601bae506f7e4db
Karl Hiner <karl.hiner@gmail.com>
2014-02-02 15:43:56 -0800

    convert SlideTab to use index buffers

--- src/com/kh/beatbot/ui/mesh/SlideTab.java
+++ src/com/kh/beatbot/ui/mesh/SlideTab.java
@@ -5,6 +5,7 @@ import com.kh.beatbot.ui.view.View;
 
 public class SlideTab extends Shape {
 	private static final int NUM_FILL_VERTICES = RoundedRect.NUM_CORNER_VERTICES * 5 * 3 * 2;
+	private static final short[] FILL_INDICES = getFillIndices();
 
 	private static RoundedRect roundedRect = new RoundedRect(null,
 			Colors.LABEL_SELECTED, null);
@@ -12,7 +13,24 @@ public class SlideTab extends Shape {
 	private float cornerRadius = 12;
 
 	public SlideTab(ShapeGroup group, float[] fillColor, float[] strokeColor) {
-		super(group, fillColor, strokeColor, NUM_FILL_VERTICES, 0);
+		super(group, fillColor, strokeColor, FILL_INDICES, null,
+				NUM_FILL_VERTICES, 0);
+	}
+
+	private static short[] getFillIndices() {
+		short[] fillIndices = new short[RoundedRect.FILL_INDICES.length * 2
+				+ RoundedRect.NUM_CORNER_VERTICES * 3 + 1];
+		for (int i = 0; i < 3; i++) {
+			short[] indices = RoundedRect.FILL_INDICES;
+			for (int j = 0; j < indices.length; j++) {
+				int index = i * indices.length + j;
+				if (index >= fillIndices.length)
+					break;
+				fillIndices[index] = (short) (indices[j] + i
+						* RoundedRect.NUM_FILL_VERTICES);
+			}
+		}
+		return fillIndices;
 	}
 
 	@Override
@@ -23,37 +41,26 @@ public class SlideTab extends Shape {
 		roundedRect.layout(x + View.mainPage.width - cornerRadius * 2, y,
 				width, height);
 
-		for (int i = 0; i < roundedRect.NUM_FILL_VERTICES; i++) {
+		for (int i = 0; i < RoundedRect.NUM_FILL_VERTICES; i++) {
 			fillVertex(roundedRect.getFillVertexX(i),
 					roundedRect.getFillVertexY(i));
 		}
 
-		roundedRect.setCornerRadius(cornerRadius);
-		roundedRect.layout(x, y, View.mainPage.width, View.mainPage.height - y);
+		roundedRect.layout(x, y, View.mainPage.width, View.mainPage.height - y
+				- cornerRadius);
 
-		for (int i = 0; i < roundedRect.NUM_FILL_VERTICES; i++) {
+		for (int i = 0; i < RoundedRect.NUM_FILL_VERTICES; i++) {
 			fillVertex(roundedRect.getFillVertexX(i),
 					roundedRect.getFillVertexY(i));
 		}
 
-		float theta = ¹, addX, addY;
-		float centerX = x + View.mainPage.width;
-		float centerY = y + height;
-		float lastX = 0, lastY = 0;
-		for (int i = 0; i < RoundedRect.NUM_CORNER_VERTICES; i++) {
-			addX = addY = cornerRadius;
-			float vertexX = (float) Math.cos(theta) * cornerRadius + addX + x
-					+ View.mainPage.width;
-			float vertexY = (float) Math.sin(theta) * cornerRadius + addY + y
-					+ height;
-			if (lastX != 0 && lastY != 0) {
-				fillVertex(vertexX, vertexY);
-				fillVertex(lastX, lastY);
-				fillVertex(centerX, centerY);
-			}
-			lastX = vertexX;
-			lastY = vertexY;
-			theta += ¹ / (RoundedRect.NUM_CORNER_VERTICES * 2);
+		roundedRect.layout(x + View.mainPage.width, y + height, width, height);
+
+		fillVertex(x + View.mainPage.width, y + height);
+
+		for (int i = RoundedRect.NUM_CORNER_VERTICES * 2 + 1; i < RoundedRect.NUM_CORNER_VERTICES * 3 + 1; i++) {
+			fillVertex(roundedRect.getFillVertexX(i),
+					roundedRect.getFillVertexY(i));
 		}
 	}
 }
