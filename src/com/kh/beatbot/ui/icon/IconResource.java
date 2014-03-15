03d9290f58685b6d6330e2b8d9eaa97e12411a81
Karl Hiner <karl.hiner@gmail.com>
2014-03-15 12:38:29 -0700

    add textColor field to IconResources to allow different color than border

--- src/com/kh/beatbot/ui/icon/IconResource.java
+++ src/com/kh/beatbot/ui/icon/IconResource.java
@@ -4,15 +4,22 @@ public class IconResource {
 	public int resourceId;
 	public float[] fillColor;
 	public float[] strokeColor;
+	public float[] textColor;
 
 	public IconResource(int resourceId, float[] fillColor, float[] strokeColor) {
+		this(resourceId, fillColor, strokeColor, null);
+	}
+
+	public IconResource(int resourceId, float[] fillColor, float[] strokeColor, float[] textColor) {
 		this.resourceId = resourceId;
 		this.fillColor = fillColor;
 		this.strokeColor = strokeColor;
+		this.textColor = textColor;
 	}
 
 	public IconResource copy() {
 		return new IconResource(resourceId, fillColor == null ? null : fillColor.clone(),
-				strokeColor == null ? null : strokeColor.clone());
+				strokeColor == null ? null : strokeColor.clone(), textColor == null ? null
+						: textColor.clone());
 	}
 }
