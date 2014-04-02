6fcc56f5c6bf25585e16b359907e3eb655813e9a
Karl Hiner <karl.hiner@gmail.com>
2014-04-01 22:59:42 -0700

    reenable non-powers-of-two GL bitmaps, since the ol Tablet needs it

--- src/com/kh/beatbot/ui/texture/TextureAtlas.java
+++ src/com/kh/beatbot/ui/texture/TextureAtlas.java
@@ -47,8 +47,8 @@ public abstract class TextureAtlas {
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
 		// allow non-power-of-2 images to render with hardware acceleration enabled
-		// gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
-		// gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
 		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
 		bitmap.recycle();
 	}
