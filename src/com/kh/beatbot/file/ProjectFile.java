bb42cf34cf2fefc9fd4007248c14b2cc14352e69
Karl Hiner <karl.hiner@gmail.com>
2015-05-24 13:09:52 -0700

    Save/Load master track settings

--- src/com/kh/beatbot/file/ProjectFile.java
+++ src/com/kh/beatbot/file/ProjectFile.java
@@ -10,12 +10,13 @@ import java.io.InputStreamReader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.kh.beatbot.manager.TrackManager;
+import com.kh.beatbot.track.BaseTrack;
 import com.kh.beatbot.track.Track;
 import com.kh.beatbot.track.TrackSerializer;
 
 public class ProjectFile {
 	private String path;
-	private final static Gson GSON = new GsonBuilder().registerTypeAdapter(Track.class, new TrackSerializer()).create();
+	private final static Gson GSON = new GsonBuilder().registerTypeAdapter(BaseTrack.class, new TrackSerializer()).create();
 
 	public ProjectFile(String path) {
 		this.path = path;
@@ -25,7 +26,7 @@ public class ProjectFile {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
 		String serializedTrack;
 		while ((serializedTrack = reader.readLine()) != null) {
-			final Track track = fromJson(serializedTrack); 
+			final BaseTrack track = (BaseTrack) fromJson(serializedTrack); 
 			//TrackManager.createTrack(track);
 		}
 		reader.close();
@@ -33,6 +34,8 @@ public class ProjectFile {
 
 	public void save() throws IOException {
 		FileOutputStream outputStream = new FileOutputStream(new File(path));
+		BaseTrack masterTrack = TrackManager.getMasterTrack();
+		outputStream.write((toJson(masterTrack) + "\n").getBytes());
 		for (Track track : TrackManager.getTracks()) {
 			String trackJson = toJson(track) + "\n";
 			outputStream.write(trackJson.getBytes());
@@ -40,12 +43,12 @@ public class ProjectFile {
 
 		outputStream.close();
 	}
-	
-	public static String toJson(Track track) {
-		return GSON.toJson(track, Track.class);
+
+	public static String toJson(BaseTrack track) {
+		return GSON.toJson(track, BaseTrack.class);
 	}
 	
-	public static Track fromJson(String serializedTrack) {
-		return GSON.fromJson(serializedTrack, Track.class);
+	public static BaseTrack fromJson(String serializedTrack) {
+		return GSON.fromJson(serializedTrack, BaseTrack.class);
 	}
 }
--- src/com/kh/beatbot/track/TrackSerializer.java
+++ src/com/kh/beatbot/track/TrackSerializer.java
@@ -21,73 +21,90 @@ import com.kh.beatbot.effect.ADSR;
 import com.kh.beatbot.manager.TrackManager;
 import com.kh.beatbot.midi.MidiNote;
 
-public class TrackSerializer implements JsonSerializer<Track>, JsonDeserializer<Track> {
+public class TrackSerializer implements JsonSerializer<BaseTrack>, JsonDeserializer<BaseTrack> {
 	private static final Gson GSON = new GsonBuilder().create();
 	private Type noteListType = new TypeToken<ArrayList<MidiNote>>() {
 	}.getType();
 
 	@Override
-	public Track deserialize(JsonElement trackJson, Type type, JsonDeserializationContext context)
+	public BaseTrack deserialize(JsonElement trackJson, Type type, JsonDeserializationContext context)
 			throws JsonParseException {
 		JsonObject object = trackJson.getAsJsonObject();
 		int id = object.get("id").getAsInt();
 		int position = object.get("position").getAsInt();
-		Track track = TrackManager.createTrack(id, position);
+		float volume = object.get("volume").getAsFloat();
+		float pan = object.get("pan").getAsFloat();
+		float pitch = object.get("pitch").getAsFloat();
+		float pitchCent = object.get("pitchCent").getAsFloat();
 
+		boolean isMaster = id == TrackManager.MASTER_TRACK_ID;
+		BaseTrack track = isMaster ? TrackManager.getMasterTrack() : TrackManager.createTrack(id, position);
+
+		track.getVolumeParam().setLevel(volume);
+		track.getPanParam().setLevel(pan);
+		track.getPitchParam().setLevel(pitch);
+		track.getPitchCentParam().setLevel(pitchCent);
+
+		if (isMaster) return track;
+
+		Track t = (Track) track;
 		try {
-			track.setSample(new File(object.get("samplePath").getAsString()));
+			t.setSample(new File(object.get("samplePath").getAsString()));
 		} catch (Exception e) {
 			Log.e(getClass().getName(), e.toString());
 		}
 		
-		track.setSampleLoopWindow(object.get("sample_loop_begin").getAsFloat(), object.get("sample_loop_end").getAsFloat());
+		t.setSampleLoopWindow(object.get("sample_loop_begin").getAsFloat(), object.get("sample_loop_end").getAsFloat());
 		List<MidiNote> notes = GSON.fromJson(object.get("notes"), noteListType);
 		for (MidiNote note : notes) {
 			note.create();
 		}
 
-		track.getVolumeParam().setLevel(object.get("volume").getAsFloat());
-		track.getPanParam().setLevel(object.get("pan").getAsFloat());
-		track.getPitchParam().setLevel(object.get("pitch").getAsFloat());
-		track.getPitchCentParam().setLevel(object.get("pitchCent").getAsFloat());
-
-		ADSR adsr = track.getAdsr();
+		ADSR adsr = t.getAdsr();
 		adsr.setStart(object.get("adsrStart").getAsFloat());
 		adsr.setAttack(object.get("adsrAttack").getAsFloat());
 		adsr.setDecay(object.get("adsrDecay").getAsFloat());
 		adsr.setSustain(object.get("adsrSustain").getAsFloat());
 		adsr.setRelease(object.get("adsrRelease").getAsFloat());
 		adsr.setPeak(object.get("adsrPeak").getAsFloat());
-		return track;
+
+		return t;
 	}
 
 	@Override
-	public JsonElement serialize(Track track, Type type, JsonSerializationContext context) {
+	public JsonElement serialize(BaseTrack track, Type type, JsonSerializationContext context) {
 		JsonObject object = new JsonObject();
-		object.addProperty("class", Track.class.getName());
 
 		object.addProperty("id", track.getId());
 		object.addProperty("position", TrackManager.getTracks().indexOf(track));
-
-		object.addProperty("samplePath", track.getCurrSampleFile().getAbsolutePath());
-
-		object.addProperty("sample_loop_begin", track.getLoopBeginParam().viewLevel);
-		object.addProperty("sample_loop_end", track.getLoopEndParam().viewLevel);
-
-		object.add("notes", GSON.toJsonTree(track.getMidiNotes()).getAsJsonArray());
 		
 		object.addProperty("volume", track.getVolumeParam().viewLevel);
 		object.addProperty("pan", track.getPanParam().viewLevel);
 		object.addProperty("pitch", track.getPitchParam().viewLevel);
 		object.addProperty("pitchCent", track.getPitchCentParam().viewLevel);
+		
+		if (track instanceof Track) {
+			Track t = (Track) track;
+			object.addProperty("class", Track.class.getName());
 
-		ADSR adsr = track.getAdsr();
-		object.addProperty("adsrStart", adsr.getStart());
-		object.addProperty("adsrAttack", adsr.getAttack());
-		object.addProperty("adsrDecay", adsr.getDecay());
-		object.addProperty("adsrSustain", adsr.getSustain());
-		object.addProperty("adsrRelease", adsr.getRelease());
-		object.addProperty("adsrPeak", adsr.getPeak());
+			object.addProperty("samplePath", t.getCurrSampleFile().getAbsolutePath());
+
+			object.addProperty("sample_loop_begin", t.getLoopBeginParam().viewLevel);
+			object.addProperty("sample_loop_end", t.getLoopEndParam().viewLevel);
+
+			object.add("notes", GSON.toJsonTree(t.getMidiNotes()).getAsJsonArray());
+
+			ADSR adsr = t.getAdsr();
+			object.addProperty("adsrStart", adsr.getStart());
+			object.addProperty("adsrAttack", adsr.getAttack());
+			object.addProperty("adsrDecay", adsr.getDecay());
+			object.addProperty("adsrSustain", adsr.getSustain());
+			object.addProperty("adsrRelease", adsr.getRelease());
+			object.addProperty("adsrPeak", adsr.getPeak());
+
+		} else {
+			object.addProperty("class", BaseTrack.class.getName());			
+		}
 
 		return object;
 	}
