9fdbe7786b3f0065a02c818c8277e37dec3b8c24
Karl Hiner <karl.hiner@gmail.com>
2015-06-14 14:37:06 -0700

    IntersectingLines - thicker lines (using rects instead of stroke), expand to fill parent

--- src/com/kh/beatbot/ui/shape/IntersectingLines.java
+++ src/com/kh/beatbot/ui/shape/IntersectingLines.java
@@ -1,19 +1,29 @@
 package com.kh.beatbot.ui.shape;
 
 public class IntersectingLines extends Shape {
-
 	float intersectX = 0, intersectY = 0;
+	public static final int NUM_FILL_VERTICES = 8;
+
+	public static final short[] FILL_INDICES = { 0, 0, 1, 3, 2, 2, 4, 4, 5, 7, 6, 6 };
 
 	public IntersectingLines(RenderGroup group, float[] fillColor, float[] strokeColor) {
-		super(group, fillColor, strokeColor, 0, 4);
+		super(group, fillColor, null, FILL_INDICES, null, NUM_FILL_VERTICES, 0);
 	}
 
 	@Override
 	protected void updateVertices() {
-		strokeVertex(this.x + intersectX, this.y);
-		strokeVertex(this.x + intersectX, this.y + height);
-		strokeVertex(this.x, this.y + intersectY);
-		strokeVertex(this.x + width, this.y + intersectY);
+		float strokeWidth = height / 124;
+		float halfStrokeWidth = strokeWidth / 2;
+
+		fillVertex(x + intersectX - halfStrokeWidth, y);
+		fillVertex(x + intersectX - halfStrokeWidth, y + height);
+		fillVertex(x + intersectX + halfStrokeWidth, y + height);
+		fillVertex(x + intersectX + halfStrokeWidth, y);
+
+		fillVertex(x, y + intersectY - halfStrokeWidth);
+		fillVertex(x, y + intersectY + halfStrokeWidth);
+		fillVertex(x + width, y + intersectY + halfStrokeWidth);
+		fillVertex(x + width, y + intersectY - halfStrokeWidth);
 	}
 
 	public void setIntersect(float intersectX, float intersectY) {
--- src/com/kh/beatbot/ui/view/control/Seekbar2d.java
+++ src/com/kh/beatbot/ui/view/control/Seekbar2d.java
@@ -17,7 +17,7 @@ public class Seekbar2d extends ControlView2dBase {
 	public synchronized void createChildren() {
 		selectColor = Color.LABEL_SELECTED;
 		initRoundedRect();
-		intersectingLines = new IntersectingLines(renderGroup, null, Color.TRON_BLUE);
+		intersectingLines = new IntersectingLines(renderGroup, Color.TRON_BLUE, null);
 		circle = new Circle(renderGroup, Color.TRON_BLUE, null);
 		selectedCircle = new Circle(renderGroup, selectColorTrans, null);
 		addShapes(intersectingLines, circle, selectedCircle);
@@ -29,8 +29,7 @@ public class Seekbar2d extends ControlView2dBase {
 	}
 
 	public synchronized void layoutChildren() {
-		intersectingLines.layout(absoluteX + BG_OFFSET, absoluteY + BG_OFFSET, width - BG_OFFSET
-				* 2, height - BG_OFFSET * 2);
+		intersectingLines.layout(absoluteX, absoluteY, width, height);
 
 		circle.setDimensions(getBgRectRadius(), getBgRectRadius());
 		selectedCircle.setDimensions(getBgRectRadius() * 2, getBgRectRadius() * 2);
@@ -58,7 +57,7 @@ public class Seekbar2d extends ControlView2dBase {
 		super.handleActionDown(id, pos);
 		params[0].setLevel(xToLevel(pos.x));
 		params[1].setLevel(yToLevel(pos.y));
-		intersectingLines.setStrokeColor(selectColor);
+		intersectingLines.setFillColor(selectColor);
 		circle.setFillColor(selectColor);
 		selectedCircle.show();
 	}
@@ -66,7 +65,7 @@ public class Seekbar2d extends ControlView2dBase {
 	@Override
 	public void handleActionUp(int id, Pointer pos) {
 		super.handleActionUp(id, pos);
-		intersectingLines.setStrokeColor(levelColor);
+		intersectingLines.setFillColor(levelColor);
 		circle.setFillColor(levelColor);
 		selectedCircle.hide();
 	}
